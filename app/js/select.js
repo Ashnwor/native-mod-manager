f50df4b350aa7369945ba5148e20d4f4fc38c3ea
Ashnwor <ashnwor@gmail.com>
2020-01-29 22:15:42 +0300

    general: some select work

--- app/js/select.js
+++ app/js/select.js
@@ -1,9 +1,34 @@
+const appName = 'arcus';
+const uname = require('username');
+const fs = require('fs');
 const con = require('electron').remote.getGlobal('console');
 const { dialog } = require('electron').remote;
+let username;
+
+(async () => {
+	username = await uname();
+})();
 
 document.getElementById('browse').addEventListener('click', () => {
 	let selectedFolder = dialog.showOpenDialogSync({
 		properties: ['openDirectory']
 	})[0];
 	document.getElementById('path').value = selectedFolder;
+});
+
+document.getElementById('done').addEventListener('click', () => {
+	if (document.getElementById('path').value !== '') {
+		// TODO: Validate given path
+		con.log('Working');
+		const game = {
+			skyrim: document.getElementById('path').value
+		};
+		const dir = `/home/${username}/.local/share`;
+		fs.writeFile(`${dir}/${appName}/config.json`, JSON.stringify(game), err => {
+			if (err) throw err;
+			con.log('The file has been saved!');
+			const conf = fs.readFileSync(`${dir}/${appName}/config.json`, 'utf8');
+			con.log(JSON.parse(conf));
+		});
+	}
 });
