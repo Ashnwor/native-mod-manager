7552637f89818cd3ef1d9e9dd9d876402a5d1846
Ashnwor <ashnwor@gmail.com>
2020-01-30 03:25:49 +0300

    misc: delete unneeded code

--- preload.js
+++ preload.js
@@ -36,15 +36,3 @@ window.titlebarFrame = () => {
 	});
 	custTitlebar;
 };
-
-window.addEventListener('DOMContentLoaded', () => {
-	const replaceText = (selector, text) => {
-		const element = document.getElementById(selector);
-		if (element) element.innerText = text;
-	};
-
-	// eslint-disable-next-line no-restricted-syntax
-	for (const type of ['chrome', 'node', 'electron']) {
-		replaceText(`${type}-version`, process.versions[type]);
-	}
-});
