43cc14de27c968c897876ee2d2a1cf88f592e8e0
Ashnwor <ashnwor@gmail.com>
2020-02-04 05:02:17 +0300

    general: append plugins to rightList, first start fix

--- app/js/app.js
+++ app/js/app.js
@@ -2,15 +2,6 @@ let dir;
 let rawConfig;
 debug = debugThis => window.globalDebug(debugThis);
 
-const newDropdownEl = (id, label) => {
-	const dropdownMenu = document.getElementById('dropdownMenu');
-	const dropdownEl = document.createElement('a');
-	dropdownEl.id = id;
-	dropdownEl.classList.add('dropdown-item');
-	dropdownEl.innerText = label;
-	dropdownMenu.appendChild(dropdownEl);
-};
-
 const getConfig = () => {
 	rawConfig = window.fs.readFileSync(`${dir}/${window.appName}/config.json`);
 	config = JSON.parse(rawConfig);
@@ -28,6 +19,70 @@ const writeConfig = () => {
 	getConfig();
 };
 
+const getPlugins = () => {
+	const newRightMenuEl = label => {
+		const rightMenu = document.getElementById('rightMenuList');
+		const newListEl = document.createElement('li');
+		newListEl.classList.add('list-group-item');
+		newListEl.innerText = label;
+		rightMenu.appendChild(newListEl);
+	};
+
+	const isItPlugin = line => {
+		if (line[0] === '#') {
+			debug(`${line} COMMENT`);
+		} else if (
+			line[0] != '*' &&
+			`${line[line.length - 3]}${line[line.length - 2]}${
+				line[line.length - 1]
+			}` === 'esp'
+		) {
+			debug(`${line} PLUGIN, NOT ACTIVE`);
+			newRightMenuEl(line);
+		} else if (
+			line[0] != '*' &&
+			`${line[line.length - 3]}${line[line.length - 2]}${
+				line[line.length - 1]
+			}` === 'esp'
+		) {
+			debug(`${line} PLUGIN, ACTIVE`);
+		}
+	};
+
+	// Get this from preload in relation to selected game
+	const skyrimSEid = 489830;
+	const pfx = `${config.skyrimSE}/../../compatdata/${skyrimSEid}/pfx`;
+	const pluginsDir = `${pfx}/drive_c/users/steamuser/Local Settings/Application Data/Skyrim Special Edition`;
+
+	debug(pluginsDir);
+	const pluginsFile = window.fs.readFileSync(
+		`${pluginsDir}/Plugins.txt`,
+		'utf8'
+	);
+
+	let lines = [];
+	pluginsFile.split(/\r?\n/).forEach(line => {
+		lines.push(line);
+	});
+	debug(lines);
+
+	for (i = 0; i < lines.length; i += 1) {
+		isItPlugin(lines[i]);
+	}
+};
+
+const newDropdownEl = (id, label) => {
+	const dropdownMenu = document.getElementById('dropdownMenu');
+	const dropdownEl = document.createElement('a');
+	dropdownEl.id = id;
+	dropdownEl.classList.add('dropdown-item');
+	dropdownEl.innerText = label;
+	dropdownEl.onclick = () => {
+		document.getElementById('dropdownLabel').innerText = label;
+	};
+	dropdownMenu.appendChild(dropdownEl);
+};
+
 // First start
 if (window.platform === 'linux') {
 	dir = `/home/${window.getUsername}/.local/share`;
@@ -36,52 +91,41 @@ if (window.platform === 'linux') {
 		debug('First time setup');
 		window.fs.mkdirSync(`${dir}/${window.appName}`);
 		window.ipcRenderer.send('open-game-select');
-	}
 
-	getConfig();
-	config.skseFound = false;
-	dirArr = window.fs.readdirSync(config['skyrimSE']);
-	debug(config);
+		getConfig();
+		config.skseFound = false;
+		dirArr = window.fs.readdirSync(config['skyrimSE']);
+		debug(config);
 
-	for (i = 0; i < dirArr.length; i += 1) {
-		if (dirArr[i].slice(0, 6) === 'skse64') {
-			config.skseFound = true;
-			// TODO: Add dropdown menu to start script extender
-			break;
+		for (i = 0; i < dirArr.length; i += 1) {
+			if (dirArr[i].slice(0, 6) === 'skse64') {
+				config.skseFound = true;
+				// TODO: Add dropdown menu to start script extender
+				break;
+			}
 		}
-	}
 
-	if (config.skseFound === true) {
-		debug(`skseFound: ${config.skseFound}`);
-		writeConfig();
-		config.dropdownMenuItems = {
-			skse: { id: 'launchSKSE', title: 'Launch SKSE' }
-		};
-		writeConfig();
-		debug(config);
-		// TODO: Add an item to dropdown menu to start script extender
-	} else if (config.skseFound === false) {
-		debug(`skseFound: ${config.skseFound}`);
-		debug(config);
-		// TODO: Add an item to dropdown menu to install script extender
+		if (config.skseFound === true) {
+			debug(`skseFound: ${config.skseFound}`);
+			writeConfig();
+			config.dropdownMenuItems = {
+				skse: { id: 'launchSKSE', title: 'Launch SKSE' }
+			};
+			writeConfig();
+			debug(config);
+			// TODO: Add an item to dropdown menu to start script extender
+		} else if (config.skseFound === false) {
+			debug(`skseFound: ${config.skseFound}`);
+			debug(config);
+			// TODO: Add an item to dropdown menu to install script extender
+		}
 	}
 }
 
 // import dropdown menu items
 getConfig();
-const skyrimSEid = 489830;
-const pfx = `${config.skyrimSE}/../../compatdata/${skyrimSEid}/pfx`;
-const pluginsDir = `${pfx}/drive_c/users/steamuser/Local Settings/Application Data/Skyrim Special Edition`;
+getPlugins();
 
-debug(pluginsDir);
-const pluginsFile = window.fs.readFileSync(`${pluginsDir}/Plugins.txt`, 'utf8');
-
-let lines = [];
-pluginsFile.split(/\r?\n/).forEach(line => {
-	lines.push(line);
-});
-
-debug(lines);
 newDropdownEl(
 	config.dropdownMenuItems.skse.id,
 	config.dropdownMenuItems.skse.title
